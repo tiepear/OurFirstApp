c79dc525cd0babe30fc815ca1234a3564e4a2126
tiepear <e175719@ie.u-ryukyu.ac.jp>
2018-11-24 14:51:52 +0900

    Initial Commit

---
+++ OurFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurFirstApp
+//
+//  Created by 新垣結梨 on 2018/11/24.
+//  Copyright © 2018 Yuiri A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
